bf8353204a8c2101004390ce4b93426294df63ba
Weslley <weslley-itabira@gmail.com>
2025-11-30 15:08:12 -0300

    Deploy custom domain fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/devices/', // Caminho base no GitHub Pages
+    base: '/', // Caminho base no GitHub Pages
 
     server: {
       port: 3000,
